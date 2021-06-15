906b0c26d49698dd7314b0fae78248c51e7fbe95
Michael Williams <m.williams.4@research.gla.ac.uk>
2021-06-15 13:47:48 +0100

    Add tests for coupling NSF

---
+++ tests/test_flows/test_nsf.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import pytest
+import torch
+
+from glasflow.flows import CouplingNSF
+
+@pytest.mark.parametrize('num_bins', [4, 10])
+def test_coupling_nsf_init(num_bins):
+    """Test the initialise method"""
+    flow = CouplingNSF(2, 2, num_bins=num_bins)
+
+
+@pytest.mark.integration_test
+def test_coupling_nsf_forward_inverse():
+    """Make sure the flow is invertible"""
+    x = torch.randn(10, 2)
+    flow = CouplingNSF(2, 2)
+
+    with torch.no_grad():
+        x_prime, log_prob = flow.forward(x)
+        x_out, log_prob_inv = flow.inverse(x_prime)
+
+    np.testing.assert_array_almost_equal(x, x_out)
+    np.testing.assert_array_almost_equal(log_prob, -log_prob_inv)

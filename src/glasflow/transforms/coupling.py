15a93826a18d9e12b65943a2913130058c45e78b
mj-will <m.williams.4@research.gla.ac.uk>
2021-11-02 17:54:23 +0000

    Add affine coupling transform with trainable log-scale

--- src/glasflow/transforms/coupling.py
+++ src/glasflow/transforms/coupling.py
@@ -2,8 +2,13 @@
 """Alternative implementations of coupling transforms"""
 from nflows.transforms.coupling import (
     AffineCouplingTransform as BaseAffineCouplingTransform,
+    CouplingTransform,
 )
+from nflows.utils import torchutils
+import torch
+from torch import nn
 import torch.nn.functional as F
+
 
 
 class AffineCouplingTransform(BaseAffineCouplingTransform):
@@ -54,3 +59,49 @@ class AffineCouplingTransform(BaseAffineCouplingTransform):
             raise RuntimeError(
                 f"Unknown scaling method: {self.scaling_method}"
             )
+
+
+class AffineCouplingTransformLogScale(CouplingTransform):
+    """Version of AffineCouplingTransform that predicts the log-scale.
+    
+    Allows for a learnable scale parameter the rescales the log-scale as
+    described here: https://uvadlc-notebooks.readthedocs.io/en/latest/tutorial_notebooks/tutorial11/NF_image_modeling.html#Coupling-layers
+    """
+
+    def __init__(
+        self,
+        mask,
+        transform_net_create_fn,
+        unconditional_transform=None,
+    ):
+        super().__init__(
+            mask,
+            transform_net_create_fn,
+            unconditional_transform=unconditional_transform,
+        )
+
+        self.scaling_factor = nn.Parameter(
+            torch.ones(self.num_transform_features)
+        )
+
+    def _transform_dim_multiplier(self):
+        return 2
+
+    def _scale_and_shift(self, transform_params):
+        log_scale = transform_params[:, self.num_transform_features:, ...]
+        shift = transform_params[:, : self.num_transform_features, ...]
+        scale_factor = self.scaling_factor.exp()
+        log_scale = torch.tanh(log_scale / scale_factor) * scale_factor
+        return log_scale, shift
+
+    def _coupling_transform_forward(self, inputs, transform_params):
+        log_scale, shift = self._scale_and_shift(transform_params)
+        outputs = inputs * torch.exp(log_scale) + shift
+        logabsdet = torchutils.sum_except_batch(log_scale, num_batch_dims=1)
+        return outputs, logabsdet
+
+    def _coupling_transform_inverse(self, inputs, transform_params):
+        log_scale, shift = self._scale_and_shift(transform_params)
+        outputs = (inputs - shift) / torch.exp(log_scale)
+        logabsdet = -torchutils.sum_except_batch(log_scale, num_batch_dims=1)
+        return outputs, logabsdet

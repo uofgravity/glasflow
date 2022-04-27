cb9977ac0eddec08c85588bb0f9895b2c7e86ea1
mj-will <m.williams.4@research.gla.ac.uk>
2022-04-27 16:50:07 +0100

    Add tests for masks

--- tests/test_flows/test_coupling.py
+++ tests/test_flows/test_coupling.py
@@ -2,9 +2,11 @@
 
 from glasflow.flows.coupling import CouplingFlow
 from glasflow.nflows.transforms.coupling import AffineCouplingTransform
+import numpy as np
 import torch
 
 import pytest
+import torch
 
 
 @pytest.mark.parametrize(
@@ -75,3 +77,39 @@ def test_coupling_flow_sample_and_log_prob_w_conditional():
     log_prob = log_prob.detach().numpy()
     assert x.shape == (n, 2)
     assert log_prob.shape == (n,)
+
+
+@pytest.mark.parametrize(
+    "mask, expected",
+    [
+        (None, torch.tensor([[-1, 1], [1, -1], [-1, 1]]).int()),
+        (
+            torch.tensor([-1, 1]),
+            torch.tensor([[-1, 1], [1, -1], [-1, 1]]).int()
+        ),
+        ([-1, 1], torch.tensor([[-1, 1], [1, -1], [-1, 1]]).int()),
+        (np.array([-1, 1]), torch.tensor([[-1, 1], [1, -1], [-1, 1]]).int()),
+        (
+            torch.tensor([[1, -1], [1, -1], [-1, 1]]),
+            torch.tensor([[1, -1], [1, -1], [-1, 1]]).int()
+        ),
+    ],
+)
+def test_validate_mask(mask, expected):
+    """Assert the correct mask is returned"""
+    out = CouplingFlow.validate_mask(mask, 2, 3)
+    assert torch.equal(out, expected)
+
+
+def test_validate_mask_invalid_length():
+    """Assert a mask that is an invalid length raises an error"""
+    with pytest.raises(ValueError) as excinfo:
+        CouplingFlow.validate_mask([1, -1, 1], 2, 3)
+    assert "does not match number of inputs" in str(excinfo.value)
+
+
+def test_validate_mask_invalid_depth():
+    """Assert a mask that is an invalid depth raises an error"""
+    with pytest.raises(ValueError) as excinfo:
+        CouplingFlow.validate_mask([[1, -1], [-1, 1]], 2, 3)
+    assert "does not match number of transforms" in str(excinfo.value)

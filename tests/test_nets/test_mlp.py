f8f0acec0be547d3b23e0c4b7e43b55f9cc0d7dd
Michael Williams <m.williams.4@research.gla.ac.uk>
2022-10-07 09:43:40 +0100

    test: add basics tests for MLP

--- tests/test_nets/test_mlp.py
+++ tests/test_nets/test_mlp.py
@@ -1,7 +1,40 @@
 """Tests for the MLP submodule"""
+from unittest.mock import create_autospec
 from glasflow.nets.mlp import MLP
 import pytest
 import torch
+
+
+@pytest.fixture
+def mlp():
+    return create_autospec(MLP)
+
+
+def test_mlp_init_hidden_layers(mlp):
+    """Make sure the correct hidden layers are added"""
+    MLP.__init__(mlp, 2, 1, [20, 10])
+
+    assert mlp._input_layer.in_features == 2
+    assert mlp._input_layer.out_features == 20
+    # One layer will be the input layer
+    assert len(mlp._hidden_layers) == 1
+    assert mlp._output_layer.in_features == 10
+    assert mlp._output_layer.out_features == 1
+
+
+@pytest.mark.parametrize("activate_output", [False, True, torch.sigmoid])
+def test_mlp_init_activate_output(mlp, activate_output):
+    """Assert the different possible inputs are valid"""
+    MLP.__init__(
+        mlp,
+        1,
+        1,
+        [
+            1,
+        ],
+        activate_output=activate_output,
+    )
+    assert mlp._activate_output is bool(activate_output)
 
 
 @pytest.mark.integration_test

baef37847f3fc45b4ec6ff3cfc16274f1afb83f9
Michael Williams <m.williams.4@research.gla.ac.uk>
2022-11-29 09:39:56 +0000

    test: integration test for uniform latent dist

--- tests/test_flows/test_nsf.py
+++ tests/test_flows/test_nsf.py
@@ -28,3 +28,28 @@ def test_coupling_nsf_forward_inverse():
 
     np.testing.assert_array_almost_equal(x, x_out)
     np.testing.assert_array_almost_equal(log_prob, -log_prob_inv)
+
+
+@pytest.mark.integration_test
+def test_coupling_nsf_uniform():
+    """Integration test to mke sure core functions work as intended with the \
+        unifrom latent space.
+    """
+    flow = CouplingNSF(2, 2, distribution="uniform")
+
+    x = torch.rand(100, 2)
+
+    with torch.no_grad():
+        z, log_j = flow.forward(x)
+        x_inv, log_j_inv = flow.inverse(z)
+        log_prob = flow.log_prob(x)
+        x_out = flow.sample(10)
+
+    # n_dims * log(1 - 0) = 0
+    # So just Jacobian
+    expceted_log_prob = log_j.numpy()
+
+    np.testing.assert_array_almost_equal(x, x_inv)
+    np.testing.assert_array_almost_equal(log_j, -log_j_inv)
+    np.testing.assert_array_equal(log_prob, expceted_log_prob)
+    assert x_out.shape == (10, 2)

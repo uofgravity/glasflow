d0da31bbc3a00d6452fc15e633658dc03148aae8
Michael Williams <m.williams.4@research.gla.ac.uk>
2022-12-02 13:23:14 +0000

    test: add basic tests for resampled Gaussian

--- tests/test_distributions/test_resampled.py
+++ tests/test_distributions/test_resampled.py
@@ -1,11 +1,11 @@
 """Tests for the resampled distributions"""
 from glasflow.distributions.resampled import ResampledGaussian
+from glasflow.nflows.distributions import StandardNormal
 from glasflow.nets.mlp import MLP
 import pytest
 import torch
 
 
-@pytest.mark.integration_test
 @pytest.mark.parametrize("trainable", [False, True])
 def test_resampled_gaussian_update_weights(trainable):
     """Assert the weights can be updated.
@@ -30,3 +30,37 @@ def test_resampled_gaussian_update_weights(trainable):
     loss = -torch.mean(dist.log_prob(x))
     loss.backward()
     dist.estimate_normalisation_constant()
+
+
+def test_log_prob_gaussian():
+    """Assert the gaussian log-probability is correct"""
+    shape = (2,)
+    dist = ResampledGaussian(shape, lambda x: 1)
+    ref_dist = StandardNormal(shape)
+    x = ref_dist.sample(10)
+    out = dist._log_prob_gaussian(x)
+    expected = ref_dist.log_prob(x)
+    torch.equal(out, expected)
+
+
+def test_sample():
+    """Assert samples are drawn with the correct shape"""
+    dims = 2
+    n = 10
+    dist = ResampledGaussian(
+        dims, lambda x: torch.ones(len(x), 1), trainable=False
+    )
+    out = dist._sample(n)
+    assert out.shape == (n, dims)
+
+
+def test_log_prob():
+    """Test the log-prob method"""
+    dims = 2
+    n = 10
+    x = torch.randn(n, 2)
+    dist = ResampledGaussian(
+        dims, lambda x: torch.ones(len(x), 1), trainable=False
+    )
+    out = dist.log_prob(x)
+    assert len(out) == n

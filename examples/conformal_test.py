c28eeb4c2ad5ff320de704a879c536a691075841
John Veitch <john.veitch@ligo.org>
2022-12-13 18:33:01 +0000

    Add transform to convert angular to cartesian coords

--- examples/conformal_test.py
+++ examples/conformal_test.py
@@ -11,11 +11,12 @@ import seaborn as sns
 from numpy import sin, cos
 from matplotlib import pyplot as plt
 import torch
+from torch import tensor as t
 matplotlib.style.use('ggplot')
 
 #get_ipython().run_line_magic('matplotlib', 'wx')
 plt.ion()
-
+import h5py
 
 # Create some training data on the surface of a sphere
 
@@ -30,55 +31,114 @@ from glasflow.flows.conformal import ConformalFlow
 from glasflow.transforms import conformal
 from glasflow.nflows import transforms
 from glasflow.nflows import distributions
+from glasflow.nflows.transforms.base import Transform
 
+
+class SphereEmbedding(Transform):
+    """
+    Transforms coordinates between polar angles and
+    catesian cordinates in a space of one higher dimension
+    Parameters:
+        - dim: int. Dimension of the input
+    """
+    def __init__(self, dim: int = 2):
+        self.dim = dim
+        super().__init__()
+    
+    def forward(self, inputs, context=None):
+        """
+        Forward transformation theta -> x
+
+        Args:
+            theta (torch.Tensor): polar coordinates
+
+        Yields:
+            x  (torch.Tensor): Euclidean coordinates
+        """
+        cos = torch.cos
+        sin = torch.sin
+        in_shape = inputs.shape
+        print('Forward input shape',in_shape)
+
+        out_shape = list(in_shape)
+        out_shape[-1] = self.dim+1
+        assert self.dim == in_shape[-1]
+        #theta = inputs.view(in_shape[0], -1)
+        outputs = inputs.new_empty(out_shape)
+        outputs[:,0] = sin(inputs[:,0])*cos(inputs[:,1])
+        outputs[:,1] = sin(inputs[:,0])*sin(inputs[:,1])
+        outputs[:,2] = cos(inputs[:,0])
+        assert torch.all(~torch.isnan(outputs))
+
+        logabsdet = inputs.new_zeros(in_shape[0])
+        
+        return outputs, logabsdet
+    
+    def inverse(self, inputs, context=None):
+        in_shape = inputs.shape
+        print('Inverse input shape',in_shape)
+        out_shape = list(in_shape)
+        out_shape[-1] = self.dim
+        assert self.dim == in_shape[-1]-1
+        outputs = inputs.new_empty(out_shape)
+        r = torch.sqrt(torch.sum(inputs[:,:]**2, axis=-1))
+        print(r.shape)
+        outputs[:,0] = torch.acos(inputs[:,2]/r)
+        outputs[:,1] = torch.atan2( inputs[:,1], inputs[:,0])
+        assert torch.all(~torch.isnan(outputs))
+        logabsdet = inputs.new_zeros(in_shape[0])
+        return outputs, logabsdet
 
 class SphereCEFlow(ConformalFlow):
-    def __init__(self, n_transforms: int, n_neurons: int = 32):
-        n = 2
-        m = 2
+    def __init__(self, dim, base_distribution = None):
+        n = dim+1
+        m = dim
+        if not base_distribution:
+            base_distribution = RealNVP(
+                 n_inputs = dim,
+                 n_transforms = 4,
+                 n_neurons = 32)
 
         conf_transform = transforms.CompositeTransform([
+            SphereEmbedding(2),
             conformal.ConformalScaleShift(n, m),
             conformal.Orthogonal(n),
             conformal.SpecialConformal(n, m),
             conformal.Pad(n, m),
         ])
-        base_flow = RealNVP(
-             n_inputs=m,
-             n_transforms=n_transforms,
-             n_neurons=n_neurons,
-         )
-        #base_flow = distributions.StandardNormal([2])
+        
 
-        super().__init__(conf_transform, distribution=base_flow)
+        super().__init__(conf_transform, distribution=base_distribution)
 
-
-# In[3]:
-
-
-# theta = norm(np.pi/2,0.1).rvs(1000)
-# phi = uniform(0,2*np.pi).rvs(1000)
-# r = 1.0
-
-# x = r*sin(theta)*cos(phi)
-# y = r*sin(theta)*sin(phi)
-# z = r*cos(theta)
-# x, y, z = map(lambda x: torch.tensor(x[None,:],dtype=torch.float32), (x,y,z))
-# sph_data = torch.cat([x,y,z]).T
-# print(sph_data.shape)
 
 from scipy.stats import halfnorm
 
+#################################
 dist = halfnorm()
 x = halfnorm.rvs(size=(500, 2))
 
-plt.scatter(x[:, 0], x[:, 1])
-plt.show()
 
-sph_data = torch.rand(1000, 2) + 100
+##################################
+## Load event samples
+t = lambda x: torch.Tensor(x)[None,:]
+filename = '/home/john/work/O3/O3a_PE_repos/o3a_catalog_events/S190513bm/Preferred/PESummary_metafile/posterior_samples.h5'
+with h5py.File(filename,'r') as posfile:
+    samps = posfile['C01:IMRPhenomPv2']['posterior_samples']
+    ra, dec, phase, psi, iota =   samps['ra'], samps['dec'], samps['phase'], samps['psi'], samps['iota']
+    x = torch.cat(( torch.pi/2 - t(dec), t(ra)
+                   #t(phase),
+                   #t(psi),
+                   #t(iota)
+                   )
+                  ).T
+
+
 sph_data = torch.FloatTensor(x)
 
-spflow = SphereCEFlow(n_transforms=4)
+plt.plot(x[:, 1], x[:, 0],'x')
+plt.show()
+
+spflow = SphereCEFlow(2) #, base_distribution = distributions.StandardNormal([2]))
 
 print("Init")
 
@@ -93,17 +153,14 @@ from torch.utils.data import DataLoader
 batch_size = 1000
 optim = opt.Adam(spflow.parameters(), lr=0.01)
 scheduler = opt.lr_scheduler.MultiStepLR(optim, milestones=[40], gamma=0.5)
-NTRAIN = 1000
+NTRAIN = 500
 def schedule():
     '''Yield epoch weights for likelihood and recon loss, respectively'''
     for _ in range(NTRAIN):
-        yield 10, 1
+        yield 10, 10000
         scheduler.step()
         
-loader = DataLoader(sph_data, batch_size=batch_size, shuffle=True, num_workers=2)
-
-
-# In[15]:
+loader = DataLoader(sph_data, batch_size=batch_size, shuffle=True, num_workers=4)
 
 
 import torch
@@ -144,11 +201,12 @@ for epoch, (alpha, beta) in enumerate(schedule()):
                                      f'| recon: {reconstruction_error:6.5f} ')
 
         #zz = mid_latent.detach()
-        zz = spflow._distribution(mid_latent)[0].detach()
-        ax.clear()
-        ax.plot(zz[:, 0], zz[:, 1],'x')
-        #plt.draw()
-        plt.pause(0.01)
+        if batch==0:
+            zz = mid_latent.detach()
+            ax.clear()
+            ax.plot(zz[:, 0], zz[:, 1],'x')
+            #plt.draw()
+            plt.pause(0.01)
 
 # with torch.no_grad():
 #     sample_mid_latent, _ = spflow._transform.forward(sph_data)
@@ -163,14 +221,16 @@ for epoch, (alpha, beta) in enumerate(schedule()):
 # ax.view_init(elev=20, azim=260)
 
 
-fig = plt.figure()
-plt.scatter(sample_mid_latent[:, 0], sample_mid_latent[:, 1])
+#fig = plt.figure()
+#plt.scatter(sample_mid_latent[:, 0], sample_mid_latent[:, 1])
 
 
 with torch.no_grad():
     gen_samples = spflow.sample(1000).detach()
 
-plt.scatter(gen_samples[:, 0], gen_samples[:, 1])
+fig = plt.figure()
+ax = fig.gca()
+ax.plot(gen_samples[:, 0], gen_samples[:, 1],'x')
 
 
 # fig = plt.figure(figsize=(8, 8))
@@ -178,15 +238,3 @@ plt.scatter(gen_samples[:, 0], gen_samples[:, 1])
 # point_plot = ax.scatter(gen_samples[:,0], gen_samples[:,1], gen_samples[:,2], color='#faab36')
 # ax.auto_scale_xyz([-1.3, 1.3], [-1.3, 1.3], [-1, 1]) # Correct aspect ratio manually
 # ax.view_init(elev=20, azim=260)
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-

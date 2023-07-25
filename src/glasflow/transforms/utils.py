baca1be2546dae20285a8572ef2cde02fee52aba
Michael Williams <m.williams.4@research.gla.ac.uk>
2023-07-25 09:42:57 +0100

    feat: support any `logN` activation

--- src/glasflow/transforms/utils.py
+++ src/glasflow/transforms/utils.py
@@ -17,14 +17,20 @@ SCALE_ACTIVATIONS = dict(
 def get_scale_activation(activation: Union[str, Callable]) -> Callable:
     """Get the scale activation function.
 
-    If `activation` is not a string, then it is returned.
+    If `activation` is not a string, then it is returned. If `activation` is
+    a string of the form `logN` where `N` is a number, then the activation is
+    `exp(2N * (sigmoid(x) - 0.5))`. In the current implementation this
+    corresponds to estimating the log-scale.
     """
     if not isinstance(activation, str):
         return activation
     elif activation in SCALE_ACTIVATIONS:
         return SCALE_ACTIVATIONS.get(activation)
+    elif activation[:3] == "log":
+        scale = float(activation[3:])
+        return lambda x: torch.exp(2 * scale * (torch.sigmoid(x) - 0.5))
     else:
         raise ValueError(
             f"Unknown activation: {activation}. "
-            f"Choose from: {SCALE_ACTIVATIONS}"
+            f"Choose from: {SCALE_ACTIVATIONS} or logN, where N is a number."
         )

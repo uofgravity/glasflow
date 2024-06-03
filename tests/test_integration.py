3759dcb65047f9f4a703d5b03653df9fa978527c
mj-will <michaeljw1@googlemail.com>
2024-06-03 15:39:52 +0100

    TST: add basic training integration test

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -3,9 +3,22 @@ Overall integration tests.
 """
 
 import pytest
+import torch
 
 
 @pytest.mark.slow_integration_test
-def test_placeholder_slow_integration():
-    """Placeholder so that CI passes"""
-    pass
+def test_flow_training(FlowClass):
+    """General integration test for all flows"""
+    n_inputs = 2
+    flow = FlowClass(n_inputs=2, n_transforms=2)
+
+    # Draw [0, 1) since some flows only support the unit interval
+    x = torch.rand(10, n_inputs)
+
+    opt = torch.optim.Adam(flow.parameters())
+
+    for param in flow.parameters():
+        param.grad = None
+    loss = -flow.log_prob(x).mean()
+    loss.backward()
+    opt.step()
